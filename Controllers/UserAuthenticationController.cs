25b3af433f9c208176af1bae88b8c2064d59e178
fiomn <fio.mn1911@gmail.com>
2023-10-21 17:56:42 -0600

    parte login y registration

---
+++ Controllers/UserAuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAplicada.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {            
+            return View();
+        }
+    }
+}
